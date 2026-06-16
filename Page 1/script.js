61f981fe09f6406abb1ae0015bb88970ba75c4db
Timothy Hestu Susanto <timothyhestu@gmail.com>
2026-06-17 01:28:07 +0700

    Update 1

--- Page 1/script.js
+++ Page 1/script.js
@@ -4,19 +4,21 @@ const authContainer = document.getElementById('auth-container');
         const registerHTML = `
             <div id="div-register" class="form-container">
                 <div class="input">
-                    <h2>Buat Akun</h2>
-                    <label class="lab-email" for="">Nama:</label>
-                    <input class="email"  type="text" placeholder><br>
-                    <label class="lab-email" for="">Telp:</label>
-                    <input class="lab-email" type="text"><br>
+                    <h2>Create Account</h2>
+                    <label class="lab-email_name" for="">Name:</label>
+                     <input class="email"  type="text" ><br>
+                    <label class="lab-email_tlp" for="">No. :</label>
+                     <input class="email" type="text" ><br>
                     <label class="lab-email" for="">Email:</label>
-                    <input class="email"  type="text" placeholder><br>
+                     <input class="email"  type="email" placeholder><br>
                     <label class="lab-password" for="">Password:</label>
-                    <input class="password" type="password"><br>
+                     <input class="password" type="password"><br>
                 </div>
                 
-                <p>Sudah punya akun?</p>
-                <a href="#" class="explore-btn2" onclick="event.preventDefault(); renderLoginForm();">Kembali ke Log In</a>
+                <a href="#" class="explore-btn3" onclick="event.preventDefault(); renderLoginForm();">Create Account</a>
+        
+                <p class="java">already have an account?</p>
+                <a href="#" class="explore-btn2" onclick="event.preventDefault(); renderLoginForm();">Back</a>
             </div>
         `;
         
@@ -27,14 +29,14 @@ const authContainer = document.getElementById('auth-container');
         const loginHTML = `
             <div id="div-login" class="form-container">
                         <div class="input">
-                            <h2>Masuk</h2>
+                            <h2>LogIn</h2>
                             <label class="lab-email" for="">Email:</label>
                             <input class="email"  type="text" placeholder><br>
                             <label class="lab-password" for="">Password:</label>
                             <input class="password" type="password"><br>
                         </div>
-                        <a href="#" class="explore-btn">Log In</a>
-                        <h4>Belum punya akun?</h4>
+                        <a href="/Page 2/page2.html" class="explore-btn">Log In</a>
+                        <p>don't have an account yet?</p>
                         <a href="#" class="explore-btn2"  onclick="event.preventDefault(); renderRegisterForm();">Create Account</a>
                     </div>
         `;
